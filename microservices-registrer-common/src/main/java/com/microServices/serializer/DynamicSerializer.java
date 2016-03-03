821fc520cfca5174e7ac145b83d4d185a67a524d
lds <lds@itesoft.com>
2016-03-03 21:24:17 +0100

    fix applicationinside stack trace

--- microservices-registrer-common/src/main/java/com/microServices/serializer/DynamicSerializer.java
+++ microservices-registrer-common/src/main/java/com/microServices/serializer/DynamicSerializer.java
@@ -1,10 +1,13 @@
 package com.microservices.serializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class DynamicSerializer<T, K>{
+    private static final Logger LOGGER = LoggerFactory.getLogger(DynamicSerializer.class);
 
     private Class<T> classTypeInput;
     private Class<K> classTypeOutPut;
@@ -59,7 +62,8 @@ public class DynamicSerializer<T, K>{
         try {
             invoiceEu = (K) this.deSerialize(json);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error(String.format("Can not deserialize [%s]. Error : %s", json, e.getMessage()), e );
+            throw new RuntimeException(e);
         }
         return invoiceEu;
     }
